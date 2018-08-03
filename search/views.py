a8dd8fb31fa4fc68378e9a2b944514053c22ca15
Peter-Lavigne <peterklavigne@gmail.com>
2018-08-03 18:52:53 -0400

    Add spotipy API

--- search/views.py
+++ search/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 def search_list_view(request):
-  return render(request, 'search/list.html', {'results': []})
+  media = request.GET.get("media")
+  query = request.GET.get("q")
+
+  results = []
+  if media == 'Music':
+    client_credentials_manager = SpotifyClientCredentials(
+      client_id=os.environ['SPOTIFY_CLIENT_ID'],
+      client_secret=os.environ['SPOTIFY_CLIENT_SECRET'],
+    )
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    for raw_result in sp.search(q=query, limit=10)['tracks']['items']:
+      results.append({
+        'name': raw_result['name'],
+        'artist': raw_result['artists'][0]['name'],  # assuming every song has at least one artist
+        'api_id': raw_result['id']
+      })
+
+  return render(request, 'search/list.html', {'media': media, 'results': results})
