5f70d38d4f73c36445ada17e9da846a00efaa06c
Peter-Lavigne <peterklavigne@gmail.com>
2018-08-04 19:39:29 -0400

    Move api calls to the boyoblue module

--- search/views.py
+++ search/views.py
@@ -1,40 +1,14 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-import os
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from boyoblue.media import songs, movies
 
 def search_view(request):
   media = request.GET.get("media")
   query = request.GET.get("q")
 
   if media == 'songs':
-    return search_songs_view(request, query)
+    return render(request, 'search/songs.html', {'songs': songs.search(query, limit=10)})
   elif media == 'movies':
-    return search_movies_view(request, query)
+    return render(request, 'search/movies.html', {'movies': movies.search(query, limit=10)})
   else:
     return HttpResponseNotFound
-
-def search_songs_view(request, query):
-
-  songs = []
-
-  client_credentials_manager = SpotifyClientCredentials(
-    client_id=os.environ['SPOTIFY_CLIENT_ID'],
-    client_secret=os.environ['SPOTIFY_CLIENT_SECRET'],
-  )
-
-  sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-  for raw_result in sp.search(q=query, limit=10)['tracks']['items']:
-    songs.append({
-      'name': raw_result['name'],
-      'artist': raw_result['artists'][0]['name'],  # assuming every song has at least one artist
-      'api_id': raw_result['id']
-    })
-
-  return render(request, 'search/songs.html', {'songs': songs})
-
-
-def search_movies_view(request, query):
-  return render(request, 'search/movies.html')
-
