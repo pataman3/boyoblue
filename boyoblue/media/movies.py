e7f7320b6b25156cf74e22a4a22a130f897285ae
Peter-Lavigne <peterklavigne@gmail.com>
2018-08-04 20:06:49 -0400

    Abstract review cards across media

--- boyoblue/media/movies.py
+++ boyoblue/media/movies.py
@@ -1,2 +1,5 @@
 def search(query, limit):
   return []
+
+def get(api_id):
+  return {}
--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Review
 from django.contrib.auth.decorators import login_required
+from boyoblue.media import songs, movies
 from . import forms
 
 def review_list_view(request):
-  reviews = Review.objects.all().order_by('date')[:10][::-1]
+  reviews = []
+  for review in Review.objects.all().order_by('date')[:10][::-1]:
+    reviews.append([review, 'reviews/mediacards/{}.html'.format(review.type)])
+    if review.type == 'song':
+      reviews[-1].append(songs.get(review.api_id))
+    elif review.type == 'movie':
+      reviews[-1].append(movies.get(review.api_id))
+    else:
+      return HttpResponseNotFound
+
   return render(request, 'reviews/list.html', {'reviews': reviews})
 
 def review_detail_view(request, api_id):
