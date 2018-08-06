2a934ffa9faa1bc5509c529e5b152639cf596feb
Peter-Lavigne <peterklavigne@gmail.com>
2018-08-06 17:01:19 -0400

    Add movie api script

--- boyoblue/media/movies.py
+++ boyoblue/media/movies.py
@@ -1,5 +1,33 @@
-def search(query, limit):
-  return []
+import os
+from tmdbv3api import TMDb, Movie
 
-def get(api_id):
-  return {}
+tmdb = TMDb()
+tmdb.api_key = os.environ['TMDB_API_KEY']
+
+movie = Movie()
+
+class Movie:
+  def __init__(self, title, api_id, poster_image):
+    self.title = title
+    self.api_id = api_id
+    self.poster_image = poster_image
+
+
+# converts the result of an API call to a Movie object
+def parse_api_call(movie):
+  return Movie(
+    title=movie.title,
+    api_id=movie.id,
+    poster_image='https://image.tmdb.org/t/p/w200/' + movie.poster_path
+  )
+
+
+def search(query, limit):
+  movies = []
+  for result in movie.search(query)[:limit]:
+    movies.append(parse_api_call(result))
+  return movies
+
+
+def get(api_key):
+  return parse_api_call(movie.details(api_key))
