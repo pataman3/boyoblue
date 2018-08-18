9275bf1f70ac0caadb0bad9f6aa76d038ed49174
Peter-Lavigne <peterklavigne@gmail.com>
2018-08-18 18:00:38 -0400

    Add pagination to searches

--- search/views.py
+++ search/views.py
@@ -5,9 +5,21 @@ from boyoblue.media import medias
 def search_view(request):
   media = request.GET.get("media")
   query = request.GET.get("q")
-  results = medias.search(media, query)
+  try:
+    page = int(request.GET.get('page', 1))
+  except ValueError:
+    page = 1
+
+  results = medias.search(media, query, page)
 
   if results is None:
     return HttpResponseNotFound
 
-  return render(request, 'search/{}.html'.format(media), {'results': results})
+  return render(request, 'search/{}.html'.format(media), {
+    'results': results,
+    'media': media,
+    'query': query,
+    'page': page,
+    'previous_page': page - 1,
+    'next_page': page + 1
+  })
