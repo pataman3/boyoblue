7640dd365d9c29abc1d81049ef5ae7281f2d534e
Peter-Lavigne <peterklavigne@gmail.com>
2018-08-31 22:36:59 -0400

    Use whitenoise for static files

--- boyoblue/settings.py
+++ boyoblue/settings.py
@@ -44,6 +44,7 @@ LOGOUT_REDIRECT_URL = 'home'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
